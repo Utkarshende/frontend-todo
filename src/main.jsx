6c9716e84e8e535f6d348443904ce9c41a45d8a2
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-09-22 12:51:53 +0530

    EditTodo file built

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import Home from './views/Home/Home.jsx';
 import NewTodo from './views/NewTodo/NewTodo.jsx';
+import EditTodo from './views/EditTodo/EditTodo.jsx';
 import {BrowserRouter, Routes ,Route} from 'react-router';
 
 const root = createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
     <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/new" element={<NewTodo />}/>
+        <Route path="/edit/:id" element={<EditTodo/>}/>
     </Routes>
     </BrowserRouter>  
 );
